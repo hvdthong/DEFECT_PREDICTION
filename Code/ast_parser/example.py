76a61c69be0e2082faa3f19efd76a99f56a32858
hvdthong <hvdthong@github.com>
2018-07-10 11:38:14 +0800

    adding example

--- Code/ast_parser/example.py
+++ Code/ast_parser/example.py
@@ -8,7 +8,7 @@ str_file = load_javafile(file=path_file)
 #     "package javalang.brewtab.com; public class Test {public static void main (String[] args){}}")
 tree = jl.parse.parse(str_file)
 for path, node in tree:
-    if str(node) == "MethodDeclaration":
-        print node, node.name
-        exit()
+    # if str(node) == "MethodDeclaration":
+    #     print node, node.name
+    #     exit()
     print node
